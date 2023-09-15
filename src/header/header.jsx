9c02f1e54e6ba1a6616f557d2f20cf798b671d6b
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-15 18:04:16 -0500

    add marquee effect on header component

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -10,7 +10,20 @@ function Header() {
           alt="ganesh Logo"
         />
 
-        <h1>"हजुर को ध्यान कता चितवन कार्ड कलेक्सन यता"</h1>
+        <marquee behavior="alternet" direction="right">
+          <h1>"हजुर को ध्यान कता चितवन कार्ड कलेक्सन यता"</h1>
+        </marquee>
+      </div>
+      <div className={styles.header}>
+        <img
+          className={styles.image}
+          src="https://w7.pngwing.com/pngs/193/452/png-transparent-lord-ganesh-thumbnail.png"
+          alt="ganesh Logo"
+        />
+
+        <marquee behavior="alternet" direction="left">
+          <h1>"हजुर को ध्यान कता चितवन कार्ड कलेक्सन यता"</h1>
+        </marquee>
       </div>
     </>
   );
